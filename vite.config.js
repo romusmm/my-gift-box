5ff7a24a8aae0f1c76b90e316c2208c39436060c
romusmm <rmsalazarm8@gmail.com>
2025-08-14 21:58:14 -0500

    build: actualizar docs para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Si tu sitio es https://usuario.github.io/<TU_REPO>/
 export default defineConfig({
   base: '/my-gift-box/',      // ← nombre del repo (ruta de GitHub Pages)
   build: { outDir: 'docs' },  // ← GitHub Pages publicará la carpeta "docs"
   plugins: [react()],
+<<<<<<< HEAD
 })
+=======
+    base: '/<my-gift-box>/',   // <- MUY IMPORTANTE para GitHub Pages de proyecto
+  build: { outDir: 'docs' } // <- genera la build dentro de /docs
+})
+>>>>>>> a662f38 (build: actualizar docs para GitHub Pages)
