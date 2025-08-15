6debf8110a92c05b044542233c9202fdd035ad64
romusmm <rmsalazarm8@gmail.com>
2025-08-14 21:56:42 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Si tu sitio es https://usuario.github.io/<TU_REPO>/
 export default defineConfig({
+  base: '/my-gift-box/',      // ← nombre del repo (ruta de GitHub Pages)
+  build: { outDir: 'docs' },  // ← GitHub Pages publicará la carpeta "docs"
   plugins: [react()],
-    base: '/<my-gift-box>/',   // <- MUY IMPORTANTE para GitHub Pages de proyecto
-  build: { outDir: 'docs' } // <- genera la build dentro de /docs
 })
